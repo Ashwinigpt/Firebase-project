dd2dc1ab47e2164859c0d9e35acf24c20c2539b0
Ashwini Gupta <ashwinigpt1996@gmail.com>
2022-12-10 12:28:58 +0530

    firebase project

---
+++ src/components/User.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function User() {
+    
+    return (
+        <div>
+            <input type="text" placeholder="Enter a Sentence"/>
+            <input type="text" placeholder="Enter a Letter"/>
+            <button>Submit</button>
+        </div>
+    )
+}
+
+export default User;
